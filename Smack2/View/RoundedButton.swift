1bdf6e66ea6b522f8b75b77f64a302dde2406371
kdoebr21 <kodeberic@yahoo.co.jp>
2018-03-17 14:01:48 +0900

    create action registerUser & create account button

---
+++ Smack2/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  Smack2
+//
+//  Created by User on 2018/03/17.
+//  Copyright © 2018年 User. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+
+}
